2e5cf734cd9445078b75bd672dfe3faae243972f
Chris Moorad <cmoorad@github.com>
2026-02-04 17:43:21 -0800

    Fix: Simplify PurgeCSS safelist to preserve object-position rules

--- purgecss.config.js
+++ purgecss.config.js
@@ -4,18 +4,8 @@ module.exports = {
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
   safelist: [
-    /spotlight\[data-align=".*"\]/, // Safelist all data-align rules (without anchor)
-    /spotlight\[data-align="top"\]/,
-    /spotlight\[data-align="center"\]/,
-    /spotlight\[data-align="bottom"\]/,
-    /spotlight\[data-align="10%"\]/,
-    /spotlight\[data-align="20%"\]/,
-    /spotlight\[data-align="30%"\]/,
-    /spotlight\[data-align="40%"\]/,
-    /spotlight\[data-align="50%"\]/,
-    /spotlight\[data-align="60%"\]/,
-    /spotlight\[data-align="70%"\]/,
-    /spotlight\[data-align="80%"\]/,
-    /spotlight\[data-align="90%"\]/,
+    // Safelist all data-align selectors for both .spotlight and a.spotlight
+    /spotlight\[data-align/,
+    /object-position/,
   ],
 };
